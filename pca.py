3405a0229495cc8807e88a1e8e625b208328d2b4
ajuneja23 <aadit.juneja12@gmail.com>
2022-11-11 19:26:26 -0600

    Added DTree Regression

--- pca.py
+++ pca.py
@@ -5,7 +5,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 import seaborn as sns
 from sklearn.decomposition import PCA
-
+from sklearn.tree import DecisionTreeRegressor
+import math
 
 
 
@@ -18,15 +19,25 @@ df=df.drop(['Unnamed: 0', 'player_name', 'team_abbreviation','player_height', 'p
 
 #print(target)
 #print(features)
-
+#NORMALIZATION
 df=pd.DataFrame(MinMaxScaler().fit_transform(df),columns=['age', 'gp', 'pts', 'reb', 'ast', 'net_rating', 'oreb_pct', 'dreb_pct', 'usg_pct', 'ts_pct', 'ast_pct'])
 #print(df)
 y=df['age']
 x=df.drop(['age'],axis=1)
 #print(y)
 #features=df.drop(['age'],axis=1)
+#PCA
 pca=PCA(n_components=3)
 principalComponents = pca.fit_transform(x)
 
 principalComponentDataFrame=pd.DataFrame(principalComponents,columns=['PC 1','PC 2','PC 3'])
-print(principalComponentDataFrame)
+#print(principalComponentDataFrame)
+x_train=principalComponentDataFrame.head(math.floor(-0.2*(principalComponentDataFrame.shape[0]))).to_numpy
+x_test=principalComponentDataFrame.tail(math.ceil(0.2*(principalComponentDataFrame.shape[0]))).to_numpy
+y_train=y.head(math.floor(-0.2*y.shape[0])).to_numpy
+y_test=y.tail(math.ceil(0,2*y.shape[0])).to_numpy
+
+regressor=DecisionTreeRegressor(random_state=0)
+regressor.fit(x_train,y_train)
+score=regressor.score(x_test,y_test)
+print(score)
